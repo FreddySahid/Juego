cd1a208f70980633a5560fb190d5484eb194367a
unknown <freddysahid@outlook.com>
2022-02-04 12:51:02 -0600

    Area

---
+++ Juego/src/sample/AreaPelota.java
@@ -0,0 +1,8 @@
+package sample;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+
+public class AreaPelota extends Pane {
+
+}
--- Juego/src/sample/ContenedorPrincipal.java
+++ Juego/src/sample/ContenedorPrincipal.java
@@ -2,5 +2,12 @@ package sample;
 import javafx.scene.layout.BorderPane;
 
 public class ContenedorPrincipal extends BorderPane{
+    private AreaPelota areaPelota;
+    public ContenedorPrincipal(){
+        areaPelota = new AreaPelota();
 
+        setCenter(areaPelota);
+
+
+    }
 }
